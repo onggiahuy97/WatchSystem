fd8964ab7235f9997e0eed6c98ff97eadd20bf7c
Huy Ong <ogh.huy@gmail.com>
2023-05-08 22:33:04 -0700

    Initial Commit

---
+++ Watch System!/Watch_System_App.swift
@@ -0,0 +1,17 @@
+//
+//  Watch_System_App.swift
+//  Watch System!
+//
+//  Created by Huy Ong on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Watch_System_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
